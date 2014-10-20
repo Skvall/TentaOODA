18ba90e605ae90233f7d1408318cffbebb7e5362
Skvall <skvall@gmail.com>
2014-10-20 10:16:53 +0200

    init with librarys and finished UseCase

---
+++ CrimeHeadlineSystemTests/TestCrimeHeadlineSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrimeHeadlineSystemTests
+{
+    [TestClass]
+    public class TestCrimeHeadlineSystem
+    {
+        [TestMethod]
+        public void Test_ReadTopCrime_on_Polisen()
+        {
+        }
+
+        [TestMethod]
+        public void Test_ReadTopCrime_on_Utryckning()
+        {
+        }
+    }
+}
